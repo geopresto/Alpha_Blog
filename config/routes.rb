dc445009f61db755d45f0cc5d4a8d551859a5f8e
Geovante Preston <geovante@rvshare.com>
2021-02-25 07:08:44 -0500

    new user signup failure

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
  root 'pages#home'
  get 'about' , to: 'pages#about'
  resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+ get 'signup', to: 'users#new'
 end
